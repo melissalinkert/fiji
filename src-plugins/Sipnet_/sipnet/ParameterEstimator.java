6802d4bb981a19e9bc020aa461d3101aa071bd07
Jan Funke <funke@ini.ch>
2011-02-16 17:18:09 +0100

    Sipnet_: finished ParameterEstimator

--- src-plugins/Sipnet_/sipnet/ParameterEstimator.java
+++ src-plugins/Sipnet_/sipnet/ParameterEstimator.java
@@ -1,5 +1,7 @@
 package sipnet;
 
+import java.util.Vector;
+
 import org.apache.commons.math.FunctionEvaluationException;
 import org.apache.commons.math.analysis.DifferentiableMultivariateRealFunction;
 import org.apache.commons.math.analysis.MultivariateRealFunction;
@@ -17,8 +19,9 @@ public class ParameterEstimator {
 	// the assignemt model to estimate paramters for
 	private AssignmentModel assignmentModel;
 
-	// the training input as a sequence of candidates
-	private Sequence        groundTruth;
+	// the training input as a sequence of candidates and according msers
+	private Sequence                  trainingSequence;
+	private Vector<Vector<Candidate>> msers;
 
 	/**
 	 * Interface class for the apache.math optimizer. Delegates computation of
@@ -101,7 +104,7 @@ public class ParameterEstimator {
 
 			double sumCosts = 0.0;
 
-			for (SequenceNode node : groundTruth)
+			for (SequenceNode node : trainingSequence)
 				sumCosts += node.getAssignment().getCosts();
 
 			return sumCosts;
@@ -119,10 +122,10 @@ public class ParameterEstimator {
 
 	}
 
-	public ParameterEstimator(Sequence groundTruth) {
+	public ParameterEstimator(Sequence trainingSequence) {
 
-		this.assignmentModel = AssignmentModel.getInstance();
-		this.groundTruth     = groundTruth;
+		this.assignmentModel  = AssignmentModel.getInstance();
+		this.trainingSequence = trainingSequence;
 	}
 
 	final public void estimate() {
@@ -151,31 +154,346 @@ public class ParameterEstimator {
 
 	final private double gradientData() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true continuation
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 1 &&
+				    assignment.getSources().get(0) != SequenceSearch.emergeNode &&
+				    assignment.getTargets().get(0) != SequenceSearch.deathNode)
+
+					sumTermsTraining +=
+							assignmentModel.dataTerm(assignment.getSources().get(0)) +
+							assignmentModel.dataTerm(assignment.getTargets().get(0));
+			}
+
+		// every true bisection
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 2)
+
+					sumTermsTraining +=
+							assignmentModel.dataTerm(assignment.getSources().get(0)) +
+							assignmentModel.dataTerm(assignment.getTargets().get(0)) +
+							assignmentModel.dataTerm(assignment.getTargets().get(1));
+
+				if (assignment.getSources().size() == 2 &&
+				    assignment.getTargets().size() == 1)
+
+					sumTermsTraining +=
+							assignmentModel.dataTerm(assignment.getTargets().get(0)) +
+							assignmentModel.dataTerm(assignment.getSources().get(0)) +
+							assignmentModel.dataTerm(assignment.getSources().get(1));
+			}
+
+		// every true end in the training data
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().get(0) == SequenceSearch.emergeNode)
+					sumTermsTraining += assignmentModel.dataTerm(assignment.getTargets().get(0));
+
+				if (assignment.getTargets().get(0) == SequenceSearch.deathNode)
+					sumTermsTraining += assignmentModel.dataTerm(assignment.getSources().get(0));
+			}
+
+		// for each possible continuation
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate source : sliceCandidates)
+				for (Candidate target : source.getMostLikelyCandidates()) {
+
+					double p = Math.exp(-assignmentModel.costContinuation(source, target, true));
+					p = p/(p + 1.0);
+
+					sumTermsExpected +=
+						(assignmentModel.dataTerm(source) +
+						 assignmentModel.dataTerm(target))*p;
+				}
+
+		// for each possible bisection
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate candidate : sliceCandidates) {
+
+				for (Candidate neighbor : candidate.mergeTargets().keySet())
+					for (Candidate target : candidate.mergeTargets().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(target, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected +=
+								(assignmentModel.dataTerm(target) +
+								 assignmentModel.dataTerm(candidate) +
+								 assignmentModel.dataTerm(neighbor))*p;
+				}
+
+				for (Candidate neighbor : candidate.splitSources().keySet())
+					for (Candidate source : candidate.splitSources().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(source, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected +=
+								(assignmentModel.dataTerm(source) +
+								 assignmentModel.dataTerm(candidate) +
+								 assignmentModel.dataTerm(neighbor))*p;
+				}
+			}
+
+		// every possible end
+		int s = 0;
+		for (Vector<Candidate> sliceCandidates : msers) {
+
+			// all but last slice
+			if (s < msers.size() - 1)
+				for (Candidate target : sliceCandidates) {
+
+					double p = Math.exp(-assignmentModel.costEnd(target));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.dataTerm(target)*p;
+				}
+
+			// all but first slice
+			if (s > 0)
+				for (Candidate source : sliceCandidates) {
+
+					double p = Math.exp(-assignmentModel.costEnd(source));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.dataTerm(source)*p;
+				}
+
+			s++;
+		}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 
 	final private double gradientPositionContinuation() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true continuation
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 1 &&
+				    assignment.getSources().get(0) != SequenceSearch.emergeNode &&
+				    assignment.getTargets().get(0) != SequenceSearch.deathNode)
+
+					sumTermsTraining +=
+							assignmentModel.centerTerm(
+									assignment.getSources().get(0),
+									assignment.getTargets().get(0));
+			}
+
+		// for each possible continuation
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate source : sliceCandidates)
+				for (Candidate target : source.getMostLikelyCandidates()) {
+
+					double p = Math.exp(-assignmentModel.costContinuation(source, target, true));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.centerTerm(source, target)*p;
+				}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 
 	final private double gradientShapeContinuation() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true continuation
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 1 &&
+				    assignment.getSources().get(0) != SequenceSearch.emergeNode &&
+				    assignment.getTargets().get(0) != SequenceSearch.deathNode)
+
+					sumTermsTraining +=
+							assignmentModel.shapeTerm(
+									assignment.getSources().get(0),
+									assignment.getTargets().get(0));
+			}
+
+		// for each possible continuation
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate source : sliceCandidates)
+				for (Candidate target : source.getMostLikelyCandidates()) {
+
+					double p = Math.exp(-assignmentModel.costContinuation(source, target, true));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.shapeTerm(source, target)*p;
+				}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 
 	final private double gradientPositionBisection() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true bisection
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 2)
+
+					sumTermsTraining +=
+							assignmentModel.centerTerm(
+									assignment.getSources().get(0),
+									assignment.getTargets().get(0),
+									assignment.getTargets().get(1));
+
+				if (assignment.getSources().size() == 2 &&
+				    assignment.getTargets().size() == 1)
+
+					sumTermsTraining +=
+							assignmentModel.centerTerm(
+									assignment.getTargets().get(0),
+									assignment.getSources().get(0),
+									assignment.getSources().get(1));
+			}
+
+		// for each possible bisection
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate candidate : sliceCandidates) {
+
+				for (Candidate neighbor : candidate.mergeTargets().keySet())
+					for (Candidate target : candidate.mergeTargets().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(target, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected += assignmentModel.centerTerm(target, candidate, neighbor)*p;
+				}
+
+				for (Candidate neighbor : candidate.splitSources().keySet())
+					for (Candidate source : candidate.splitSources().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(source, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected += assignmentModel.centerTerm(source, candidate, neighbor)*p;
+				}
+			}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 
 	final private double gradientShapeBisection() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true bisection
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().size() == 1 &&
+				    assignment.getTargets().size() == 2)
+
+					sumTermsTraining +=
+							assignmentModel.shapeTerm(
+									assignment.getSources().get(0),
+									assignment.getTargets().get(0),
+									assignment.getTargets().get(1));
+
+				if (assignment.getSources().size() == 2 &&
+				    assignment.getTargets().size() == 1)
+
+					sumTermsTraining +=
+							assignmentModel.shapeTerm(
+									assignment.getTargets().get(0),
+									assignment.getSources().get(0),
+									assignment.getSources().get(1));
+			}
+
+		// for each possible bisection
+		for (Vector<Candidate> sliceCandidates : msers)
+			for (Candidate candidate : sliceCandidates) {
+
+				for (Candidate neighbor : candidate.mergeTargets().keySet())
+					for (Candidate target : candidate.mergeTargets().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(target, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected += assignmentModel.shapeTerm(target, candidate, neighbor)*p;
+				}
+
+				for (Candidate neighbor : candidate.splitSources().keySet())
+					for (Candidate source : candidate.splitSources().get(neighbor)) {
+
+						double p = Math.exp(-assignmentModel.costBisect(source, candidate, neighbor));
+						p = p/(p + 1.0);
+
+						sumTermsExpected += assignmentModel.shapeTerm(source, candidate, neighbor)*p;
+				}
+			}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 
 	final private double gradientEnd() {
 
-		return 0.0;
+		double sumTermsTraining = 0.0;
+		double sumTermsExpected = 0.0;
+
+		// every true end in the training data
+		for (SequenceNode node : trainingSequence)
+			for (SingleAssignment assignment : node.getAssignment()) {
+
+				if (assignment.getSources().get(0) == SequenceSearch.emergeNode)
+					sumTermsTraining += assignmentModel.endTerm(assignment.getTargets().get(0));
+
+				if (assignment.getTargets().get(0) == SequenceSearch.deathNode)
+					sumTermsTraining += assignmentModel.endTerm(assignment.getSources().get(0));
+			}
+
+		// every possible end
+		int s = 0;
+		for (Vector<Candidate> sliceCandidates : msers) {
+
+			// all but last slice
+			if (s < msers.size() - 1)
+				for (Candidate target : sliceCandidates) {
+
+					double p = Math.exp(-assignmentModel.costEnd(target));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.endTerm(target)*p;
+				}
+
+			// all but first slice
+			if (s > 0)
+				for (Candidate source : sliceCandidates) {
+
+					double p = Math.exp(-assignmentModel.costEnd(source));
+					p = p/(p + 1.0);
+
+					sumTermsExpected += assignmentModel.endTerm(source)*p;
+				}
+
+			s++;
+		}
+
+		return sumTermsExpected - sumTermsTraining;
 	}
 }
