5af13f0b16e6837aa682d442299c6801e7b5a2d2
Jan Funke <funke@ini.ch>
2011-02-21 11:07:36 +0100

    Sipnet_: bugfix - cost computation for OneToOneAssignments did not take care of emerge/death

--- src-plugins/Sipnet_/sipnet/OneToOneAssignment.java
+++ src-plugins/Sipnet_/sipnet/OneToOneAssignment.java
@@ -12,6 +12,11 @@ public class OneToOneAssignment extends SingleAssignment {
 
 		AssignmentModel assignmentModel = AssignmentModel.getInstance();
 
+		if (getSources().get(0) == SequenceSearch.emergeNode)
+			return assignmentModel.costEnd(getTargets().get(0));
+		if (getTargets().get(0) == SequenceSearch.deathNode)
+			return assignmentModel.costEnd(getSources().get(0));
+
 		return assignmentModel.costContinuation(
 				getTargets().get(0),
 				getSources().get(0),
